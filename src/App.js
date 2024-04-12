f774d8459c08463cafdb718cfaee4ac44e212c03
James Leahy Stephens <jaystephens1124@gmail.com>
2024-04-12 13:41:47 -0400

    small changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    /*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +18,10 @@ function App() {
           Learn React
         </a>
       </header>
+    </div>*/
+    <div>
+      <h1>My First React App</h1>
+      <p>Welcome :)</p>
     </div>
   );
 }
